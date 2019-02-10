cfbafb22d5f60a7338d9db16f52e5282979aa173
wesley sprague <wesleysprague@yahoo.com>
2019-02-10 13:50:06 -0500

    fixed bug in tests

--- project/frontend/cypress/integration/app_spec.js
+++ project/frontend/cypress/integration/app_spec.js
@@ -5,29 +5,25 @@ describe("Django REST framework / React quickstart app", () => {
     message: "I have been looking for love in the wrong places"
   };
 
-  before( () => {
+  before(() => {
     cy.exec("npm run dev");
     cy.exec("npm run flush");
   });
-
   it("should be able to fill a web form", () => {
     cy.visit("/");
-
     cy
       .get('input[name="name"]')
       .type(lead.name)
       .should("have.value", lead.name);
-
     cy
       .get('input[name="email"]')
       .type(lead.email)
       .should("have.value", lead.email);
-
     cy
       .get('textarea[name="message"]')
       .type(lead.message)
       .should("have.value", lead.message);
-
     cy.get("form").submit();
   });
+  // more tests here
 });
